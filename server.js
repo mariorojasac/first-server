d7f6df1d7039e8b7640ae091516077d99964841c
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-09-26 20:27:17 -0400

    second commit

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-const express = require("express")
+// require our dependecies
+const { response } = require('express')
+const express = require('express')
 
- const app = express();
+// initilaize the express app 
+const app = express()
 
- app.get("/", (req, res) => {
-   res.send("Hello World!");
- });
+// define some routes and responses to those routes
+app.get("/", (req, res) => {
+    console.log("Oh hey! I got a request. Let me respond with something")
+    res.send("Hello World!")
+})
 
- app.listen(3000);
+// heres our new route definition!
+app.get("/somedata", (request, response) => {
+    response.send("here is your information")
+})
+
+
+// tell express to listen for requesr from the browser aka client 
+
+app.listen(3000, () => {
+    console.log("express is listening for requests from the browser")
+})
